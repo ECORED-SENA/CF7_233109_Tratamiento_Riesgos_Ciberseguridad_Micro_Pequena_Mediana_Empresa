cd780c81dd302e584e249a4472b522b367487c6b
emilydev <emilse.a.b@gmail.com>
2023-06-28 10:08:51 -0500

    Update Introduccion.vue

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -17,7 +17,7 @@
         figure.p-5
           .video
             iframe(width="560" height="315" src="https://www.youtube.com/embed/tb-5JFFA22Y" title="Auditorías a las estrategias de ciberseguridad" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-          figcaption Video. Auditorías a las estrategias de ciberseguridad
+          figcaption.position-relative Video. Auditorías a las estrategias de ciberseguridad
 
 </template>
 
